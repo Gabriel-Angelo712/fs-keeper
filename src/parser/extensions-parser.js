9b685ea2d767f0f95d3bbec5982a125b00c82cff
GabrielAngelo <gabrielangelo200367@gmail.com>
2026-07-02 00:07:42 +0100

    feat: implementação de parsing de extensões fornecidas via --extensions

--- src/parser/extensions-parser.js
+++ src/parser/extensions-parser.js
@@ -3,8 +3,10 @@ import DomainError from "../utils/errors/domain-error.js";
 
 export default class ExtensionsParser {
   #args;
+  #flag;
   constructor(args) {
     this.#args = args;
+    this.#flag = this.#extractFlag(this.#args);
   }
 
   #extractFlag(args) {
@@ -19,36 +21,42 @@ export default class ExtensionsParser {
     let openBracketIndex = arg.indexOf("[");
     let closeBracketIndex = arg.indexOf("]");
     let result = arg.slice(openBracketIndex, closeBracketIndex);
-    return result.length === 0 ? false : true;
+    return result.length === 0 ? true : false;
   }
 
   parse() {
-    new Log().info("Parsing extensions...");
+    new Log().info(`Parsing extensions...`);
+    const extensionsArray = this.#flag
+      .slice(this.openBracketIndex + 1, this.closeBracketIndex)
+      .split(" ");
+    new Log().info(`Extensions Successfuly parsed`);
+    new Log().info(`Parsed extensions: ${extensionsArray}`);
+    return extensionsArray;
   }
 
   areExtensionsProvided(fn) {
-    const FLAG = this.#extractFlag(this.#args);
     let result = false;
 
-    if (FLAG) {
+    if (this.#flag) {
       result =
-        this.#isContentWrapped(FLAG) && this.#isExtensionArrayEmpty(FLAG);
+        this.#isContentWrapped(this.#flag) &&
+        !this.#isExtensionArrayEmpty(this.#flag);
       if (result) {
-        new Log().info("Extensions were correctly provided");
+        this.openBracketIndex = this.#flag.indexOf("[");
+        this.closeBracketIndex = this.#flag.indexOf("]");
+        new Log().info(`Extensions were correctly provided`);
+        fn();
         return result;
       }
       new Log().info(
-        "Extensions were not correctly provided or extensions array is empty",
+        `Extensions were not correctly provided or extensions array is empty`,
       );
-      new Log().info("Using default extensions");
-      fn();
+      new Log().info(`Using default extensions`);
       return result;
     }
-    new Log().info(
-      "Extensions were not provided or extensions array is empty",
-    );
-    new Log().info("Using default extensions");
-    fn();
+    new Log().info(`Extensions were not provided or extensions array is empty`);
+    new Log().info(`Using default extensions`);
+    // fn();
     return result;
   }
 }
